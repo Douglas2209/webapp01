ef348b5a474f8f51093c3a82e34e2c92c16ae6ce
Douglas2209 <111157772+Douglas2209@users.noreply.github.com>
2022-08-12 20:40:46 -0300

    Update webapp01.py

--- webapp01.py
+++ webapp01.py
@@ -21,7 +21,7 @@ st.sidebar.write("Texto Side Bar")
     
 if choice == "Catalogo_de_Vendas":       
     st.image(image01, width=400, caption='Imagem ilustrativa do produto')
-    Texto01 = '<p style="font-family:tahoma; color:Blue; font-size: 18px;">A seguir, serão apresentados a você os 5 passos principais para você iniciar, rapidamente, a programar em C++. Espero que faça bom uso!</p>'
+    Texto01 = '<p style="font-family:tahoma; color:Blue; font-size: 18px;">R$ 29,99 acima de 10 unidades 27,99'
     st.markdown(Texto01, unsafe_allow_html=True)
     st.subheader("R$ 29,99 acima de 10 unidades 27,99")
     st.write("Catalogo de vendas")    
